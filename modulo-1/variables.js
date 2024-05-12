72dffb8f8ec74779e458e0e925b3d90926d5223f
Daniel Personal <danielpersonal@danielpb.local>
2024-05-11 23:51:24 -0400

    adding variables

---
+++ modulo-1/variables.js
@@ -0,0 +1,10 @@
+let edad = 26
+const nombre = "Daniel"
+let ocupacion = "Ingeniero de Software"
+const totalDeOjos = 2
+const totalDePiernas = 2
+
+edad = 27
+ocupacion = "Editor de Videos"
+
+console.log(`Mi nombre es ${nombre}, tengo ${edad} años de edad. Soy ${ocupacion}. Tengo ${totalDeOjos} ojos y ${totalDePiernas} piernas.`);
